7b93bcec6e4dd2feefde7a412618ff5c21ec7858
devhector <hectorhu17@gmail.com>
2021-04-03 10:54:19 -0300

    create product schema

---
+++ server/database/models/product.js
@@ -0,0 +1,12 @@
+import {Schema, model} from 'mongoose';
+
+const ProductSchema = new Schema({
+
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true }
+
+});
+
+module.exports = model('Product', ProductSchema);
